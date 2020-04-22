75d703225f6ab18e5b3d8b3aee304a6bb6f1e07d
VK <deadlyspider420@gmail.com>
2020-04-23 04:16:07 +0530

    Added viewset to profile api.

--- profiles_app/urls.py
+++ profiles_app/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 
-from profiles_app.views import HelloApiView
+from rest_framework.routers import DefaultRouter
+
+from profiles_app.views import HelloApiView, HelloViewSet
+
+router = DefaultRouter()
+router.register('hello-viewset', HelloViewSet, basename='hello-viewset')
 
 urlpatterns = [
-    path('', HelloApiView.as_view())
+    path('hello-view/', HelloApiView.as_view()),
+    path('', include(router.urls))
+
 ]
--- profiles_app/views.py
+++ profiles_app/views.py
@@ -1,10 +1,77 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+
+
+from profiles_app.serializers import HelloSerializer
+
 
 class HelloApiView(APIView):
+    serializer_class = HelloSerializer
+
     def get(self, request, format=None):
         list = ['Hello','World']
 
         return Response(
         {'message': 'Hey!', 'list': list}
         )
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        return Response(serializer.errors,
+                        status = status.HTTP_400_BAD_REQUEST
+                        )
+
+    def put(self, request, pk=None):
+        return Response({'message': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'message': 'PATCH'})
+
+    def delete(self, request):
+        return Response({'message': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+
+    serializer_class = HelloSerializer
+
+    def list(self, request):
+        a_viewlist = [
+        'Uses action (list, create, retrieve, update, partial_update)',
+        'Automatically maps to URLs using Routers.',
+        'Provides more functionality with less code.'
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewlist': a_viewlist})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+
+        return Response(
+        serializer.errors,
+        status = status.HTTP_400_BAD_REQUEST
+        )
+
+    def retrieve(self, request, pk=None):
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        return Response({'http_method': 'DELETE'})
